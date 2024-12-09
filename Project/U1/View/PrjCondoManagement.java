6c2cf05481f99dc43dc4abff66c266df11d0fca4
Jardel Alexander Maza Flores <2432jardel.maza@gmail.com>
2024-12-09 18:38:21 -0500

    Code Restructuring

--- Project/U1/View/PrjCondoManagement.java
+++ Project/U1/View/PrjCondoManagement.java
@@ -2,7 +2,6 @@ package ec.espe.edu.condomanagement.view;
 
 import ec.espe.edu.condomanagement.model.*;
 import java.text.ParseException;
-
 import java.util.InputMismatchException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -56,7 +55,6 @@ public class PrjCondoManagement {
         }
     }
     
-
     private static void saveData() {
         FileManager.saveAdministrator(admin, "admin.json");
         System.out.println("Data saved successfully.");
@@ -99,8 +97,7 @@ public class PrjCondoManagement {
            }
         default -> System.out.println("Invalid choice.");
     }
-}
-        
+}   
     }
 
     private static void generateResidentUtilityBill() {
