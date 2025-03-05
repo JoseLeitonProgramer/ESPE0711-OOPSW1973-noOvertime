a91eb7164302d69ebae6d64b807524250ef63a64
Klever David Jami Jami <kleverdavid165@gmail.com>
2025-03-05 10:44:12 -0500

    update

--- 05 - Code/U2/Condo Management V3/CondoManagementU2V3/CondoManagementU2/src/main/java/ec/edu/espe/condomanagementu2/controller/PrintManager.java
+++ 05 - Code/U2/Condo Management V3/CondoManagementU2V3/CondoManagementU2/src/main/java/ec/edu/espe/condomanagementu2/controller/PrintManager.java
@@ -1,37 +1,64 @@
-
 package ec.edu.espe.condomanagementu2.controller;
 
 import java.awt.print.*;
 import java.text.MessageFormat;
 import javax.swing.JTable;
 import javax.swing.JOptionPane;
+
 /**
- *
- * @author Alexander Maza
+ * Class to handle printing functionality for tables.
+ * Author: Alexander Maza
  */
 public class PrintManager {
 
+    /**
+     * Method to print the given JTable.
+     * @param table The table to be printed.
+     */
     public static void printTable(JTable table) {
         try {
-<<<<<<< HEAD
-
             PrinterJob job = PrinterJob.getPrinterJob();
 
-=======
-          
-            PrinterJob job = PrinterJob.getPrinterJob();
-
-
->>>>>>> 13402ff0e4d20549e00a7a6c930c8a1c12f28317
+            // Show print dialog to the user
             if (job.printDialog()) {
 
+                // Set header and footer formats for the print job
                 MessageFormat header = new MessageFormat("Lista de Vehículos / Residentes");
                 MessageFormat footer = new MessageFormat("Página {0}");
 
-
+                // Attempt to print the table
                 boolean complete = table.print(JTable.PrintMode.FIT_WIDTH, header, footer);
 
-
+                // Show appropriate message based on the print result
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
                 if (complete) {
                     JOptionPane.showMessageDialog(null, "Impresión completada correctamente.");
                 } else {
